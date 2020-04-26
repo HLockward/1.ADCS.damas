a098f12c01cc61936327774eefe0d8369811f2e8
Harold Lockward <HLockward12@gmail.com>
2020-04-26 23:19:36 +0200

    adding test to GameTest
    added testRemovePieceAfterNotEatingWithWhitePieces
    added testRemovePieceAfterNotEatingWithBlackPieces

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameTest.java
@@ -1,4 +1,7 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class GameTest {
 
@@ -12,5 +15,63 @@ public class GameTest {
     protected void setExpectedGame(Color color, String... strings) {
         this.expectedGame = new GameBuilder().color(color).rows(strings).build();
     }
+
+    private void expectMove(Coordinate origin, Coordinate destiny){
+        assertNull(this.game.move(origin, destiny));
+        assertEquals(this.game, this.expectedGame);
+    }
+
+    @Test
+    public void testRemovePieceAfterNotEatingWithWhitePieces(){
+        this.setGame(Color.WHITE,
+            "        ",
+            "        ",
+            "        ",
+            "    n   ",
+            "     b  ",
+            "        ",
+            "        ",
+            "        ");
+        this.setExpectedGame(Color.BLACK,
+            "        ",
+            "        ",
+            "    n   ",
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        ");
+        this.expectMove(
+            new Coordinate(4, 5),
+            new Coordinate(5, 6)
+        );
+    }
+
+    @Test
+    public void testRemovePieceAfterNotEatingWithBlackPieces(){
+        this.setGame(Color.WHITE,
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "    n   ",
+            "     b  ",
+            "        ");
+        this.setExpectedGame(Color.BLACK,
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "     b  ",
+            "        ");
+        this.expectMove(
+            new Coordinate(6, 5),
+            new Coordinate(5, 6)
+        );
+    }
+
 
 }
